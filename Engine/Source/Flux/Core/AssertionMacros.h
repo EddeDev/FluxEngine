cff3f3e0a17fb8b4deddf744afd1bcbabfaa3d5a
Edvin Pettersson <eddedev@hotmail.com>
2023-05-06 13:40:13 +0200

    Update AssertionMacros.h

--- Engine/Source/Flux/Core/AssertionMacros.h
+++ Engine/Source/Flux/Core/AssertionMacros.h
@@ -7,13 +7,13 @@
 #endif
 
 #ifdef FLUX_ENABLE_ASSERTS
-	#define FLUX_ASSERT(expression, ...) if (!(expression)) ::Flux::Logger::Assert(__VA_ARGS__)
+	#define FLUX_ASSERT(expression, ...) if (!(expression)) ::Flux::Logger::AssertionFailed(__VA_ARGS__)
 #else
 	#define FLUX_ASSERT(expression, ...)
 #endif
 
 #ifndef FLUX_BUILD_SHIPPING
-	#define FLUX_VERIFY(expression, ...) if (!(expression)) ::Flux::Logger::Verify(__VA_ARGS__)
+	#define FLUX_VERIFY(expression, ...) if (!(expression)) ::Flux::Logger::VerifyFailed(__VA_ARGS__)
 #else
 	#define FLUX_VERIFY(expression, ...)
 #endif
