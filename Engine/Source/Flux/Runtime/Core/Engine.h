9be4426a16be3c67246c21af37edf3c12f87e581
Edvin Pettersson <eddedev@hotmail.com>
2024-10-08 15:35:39 +0200

    Update Engine.h

--- Engine/Source/Flux/Runtime/Core/Engine.h
+++ Engine/Source/Flux/Runtime/Core/Engine.h
@@ -105,7 +105,7 @@ namespace Flux {
 		std::mutex m_MainThreadMutex;
 
 		std::atomic<bool> m_Running = true;
-		bool m_Minimized = false;
+		std::atomic<bool> m_Minimized = false;
 		bool m_RestartOnClose = false;
 
 		bool m_VSync = true;
